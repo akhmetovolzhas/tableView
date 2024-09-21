e3719a1a3634d6f16b43f3e86bfdf363cacc752f
Olzhas Akhmetov <olzhas@192-168-5-87.local>
2024-09-21 16:58:34 +0500

    first commit

---
+++ tableView/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  tableView
+//
+//  Created by Olzhas Akhmetov on 16.09.2024.
+//
+
+import Foundation
+
+struct Person {
+    var name = ""
+    var surname = ""
+    var imagename = ""
+}
---
+++ tableView/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Olzhas Akhmetov on 16.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var surnameLabel: UILabel!
+    
+    var person = Person()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        nameLabel.text = person.name
+        surnameLabel.text = person.surname
+        imageView.image = UIImage(named: person.imagename)
+    }
+
+
+}
+
